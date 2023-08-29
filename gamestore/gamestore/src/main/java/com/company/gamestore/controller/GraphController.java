7b3dbe2d5dcc4f54bd8aec426988c1a1fa5c9298
Brian Ruiz <98550010+bruizbruiz@users.noreply.github.com>
2023-08-29 18:03:01 -0400

    Added graphql

--- gamestore/gamestore/src/main/java/com/company/gamestore/controller/GraphController.java
+++ gamestore/gamestore/src/main/java/com/company/gamestore/controller/GraphController.java
@@ -1,4 +1,61 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Console;
+import com.company.gamestore.model.Game;
+import com.company.gamestore.repository.ConsoleRepository;
+import com.company.gamestore.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
 public class GraphController {
+    @Autowired
+    GameRepository gameRepository;
+
+    @Autowired
+    ConsoleRepository consoleRepository;
+
+    @QueryMapping
+    public List<Game> findAllGames() {
+        return gameRepository.findAll();
+    }
+
+    @QueryMapping
+    public Game findGameById(@Argument Integer id) {
+        return gameRepository.findById(id).get();
+    }
+
+    @QueryMapping
+    public List<Game> findGameByTitle(@Argument String title) {
+        return gameRepository.findByTitle(title);
+    }
+
+    @QueryMapping
+    public List<Game> findGameByEsrbRating(@Argument String esrb_rating) {
+        return gameRepository.findByEsrbRating(esrb_rating);
+    }
+
+    @QueryMapping
+    public List<Game> findGameByStudio(@Argument String studio) {
+        return gameRepository.findByStudio(studio);
+    }
+
+    @QueryMapping
+    public Console findConsoleById(@Argument Integer id) {
+        return consoleRepository.findById(id).get();
+    }
+
+    @QueryMapping
+    public List<Console> findAllConsoles() {
+        return consoleRepository.findAll();
+    }
+
+    @QueryMapping
+    public List<Console> findConsoleByManufacturer(@Argument String manufacturer) {
+        return consoleRepository.findByManufacturer(manufacturer);
+    }
 }
